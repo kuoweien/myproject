f024b4df6367615fb5c0707f475715509ea4192e
WeienKuo <aasdfangela@gmail.com>
2017-03-07 16:25:46 +0800

    for example

---
+++ src/oo/abstraction/Car.java
@@ -0,0 +1,14 @@
+package oo.abstraction;
+
+public class Car {
+	String brand;
+	String name;
+	int cc;
+	String type;
+	//public Car(String brand, String name, int cc, String type){
+		//this.brand = brand;
+		
+		
+	//}
+
+}
