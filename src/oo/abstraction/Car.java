e588391bd968b88a9689f43383079d15b144b4ab
user <user@es302n25>
2017-03-14 15:56:31 +0800

    新增建構子

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -5,10 +5,15 @@ public class Car {
 	String name;
 	int cc;
 	String type;
-	//public Car(String brand, String name, int cc, String type){
-		//this.brand = brand;
+	int status;
+	
+	public Car(String brand,String name,int cc){
+		this.brand = brand;
+		this.name = name;
+		this.cc = cc;
+		this.type = type;
 		
-		
-	//}
+	}
+	
 
 }
