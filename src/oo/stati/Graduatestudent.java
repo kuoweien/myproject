5fa4f8f0daf21e795fd994222d9d126833a41807
user <user@es302n25>
2017-05-02 16:30:56 +0800

    加入ArrayList

--- src/oo/stati/Graduatestudent.java
+++ src/oo/stati/Graduatestudent.java
@@ -1,10 +1,14 @@
 package oo.stati;
 
 public class Graduatestudent extends Student{
+	int thesis;
 	public Graduatestudent(int english,int math, int chinese){
 		this.chinese = chinese;
 		this.english = english;
 		this.math = math;
 	}
-	
+	@Override
+	public void print(){
+		System.out.println(getScore(english)+"\t"+getScore(math)+"\t"+getScore(chinese)+"\t"+thesis);
+	}
 }
--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -1,5 +1,8 @@
 package oo.stati;
 
+
+import java.util.ArrayList;
+
 public class Tester {
 
 	public static void main(String[] args) {
@@ -7,12 +10,21 @@ public class Tester {
 		Student s1 = new Student(87, 89, 65);
 		Student s2 = new Student(65, 59, 85);
 		Graduatestudent gts1 = new Graduatestudent(65, 59, 85);
+		Graduatestudent gts2 = new Graduatestudent(85, 91, 55);
 		s1.pass = 70;
 		s1.print();
 		s2.print();
 		gts1.print();
 		
-
+		ArrayList<Student> list = new ArrayList<>();
+		list.add(new Student(87, 89, 65));
+		list.add(new Student(65, 59, 85));
+		list.add(new Graduatestudent(65, 59, 85));
+		list.add(new Graduatestudent(85, 91, 55));
+		for(int i = 0;i<list.size();i++){
+			Student stu = list.get(i);
+			stu.print();
+		}
 	}
-
+	
 }
