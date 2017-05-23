ba0ffcf7b8f161ddbdcdca9d605c3fea2462088e
user <user@es302n25>
2017-05-23 16:28:48 +0800

    FileOutputStream

---
+++ src/oo/io/SimpleWrite.java
@@ -0,0 +1,21 @@
+package oo.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SimpleWrite {
+
+	public static void main(String[] args) {
+		try{
+		FileOutputStream fos = new FileOutputStream("data.txt");
+		fos.write(55);
+		fos.flush();
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
