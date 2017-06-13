27255dd21c9219d92613bc7a7f06d248c9ed0213
user <user@es302n25>
2017-06-13 16:20:03 +0800

    Commit

---
+++ src/oo/test/Member.java
@@ -0,0 +1,24 @@
+package oo.test;
+
+public class Member {
+	String id;
+	private int age;
+	String name;
+	
+	public Member(String name){
+		System.out.println("Member's Construstor");
+		this.name = name;
+	}
+	public Member(){
+	}
+	
+	public void setAge(int age){
+		if(age <0){
+			age = 0;
+		}
+		this.age = age;
+	}
+	public int getAge(){
+		return age;
+	}
+}
---
+++ src/oo/test/MemberTester.java
@@ -0,0 +1,15 @@
+package oo.test;
+
+public class MemberTester {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.setAge(-5);
+		System.out.println(member.getAge());
+		
+		//PhoneMember
+		PhoneMember member2 = new PhoneMember(); 
+		PhoneMember member3 = new PhoneMember("name"); 
+	}
+
+}
